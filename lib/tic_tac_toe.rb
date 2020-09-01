15a1a40ae6b1b1dc778165a55fd787dbfd2867b0
Alla Kuzminova <alchik@gmail.com>
2020-09-01 21:47:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,3 +42,16 @@ end
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  while !valid_move?(board,index)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+  end
+  move(board, index, "X")
+  display_board(board)
+end
