c9fe20cde84baea6599eb414c73a479d36a40d76
Alla Kuzminova <alchik@gmail.com>
2020-09-01 21:40:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,3 +16,12 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} "+"|"+" #{board[7]} "+"|"" #{board[8]} "
 end
+
+def input_to_index(input)
+  index = input.to_i
+  index - 1
+end
+
+def move(board, index, value="X")
+  board[index] = value
+end
