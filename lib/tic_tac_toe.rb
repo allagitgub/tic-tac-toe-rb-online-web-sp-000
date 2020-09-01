9fd653c67dae080c57aba283f0fd25cde02b4197
Alla Kuzminova <alchik@gmail.com>
2020-09-01 21:47:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,5 +40,5 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  index >= 0 && index <= 8 && !position_taken?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
